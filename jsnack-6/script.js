4ce31e6b37641e802b0fe55284a24e7047ba8435
lorenzotade <lorenzo.tade@gmail.com>
2021-04-27 14:28:35 +0200

    checkpoint 1

---
+++ jsnack-6/script.js
@@ -0,0 +1,6 @@
+/* 1- creo un array e lo riempio di elementi */
+/* 2- creo una funzione che prenda come parametro l'array,
+  un numero 'a' e un numero 'b' più grande, ma grande al
+  massimo quanto la lunghezza dell'array */
+/* 3- la funzione mi restituisce un nuovo array con i valori
+  dell'array passato compresi tra 'a' e 'b' */
